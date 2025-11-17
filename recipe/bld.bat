52e3c14a9358a88f54cb32c354aebae23b6922c9
Steven <scrass@anaconda.com>
2025-11-17 11:23:29 -0700

    remove files

--- recipe/bld.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
-if errorlevel 1 exit 1
--- recipe/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e  # Exit on any error
-
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
